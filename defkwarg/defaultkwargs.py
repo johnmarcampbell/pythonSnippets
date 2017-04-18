1cd3b6ddc56e56ee10594cc2925f149cac0f6cdd
John Campbell <johnmarcampbell@gmail.com>
2017-04-18 14:01:00 -0400

    Add docstring to @defaultkwargs

--- defkwarg/defaultkwargs.py
+++ defkwarg/defaultkwargs.py
@@ -10,13 +10,30 @@ def _restrictive_update(old_dict, new_dict):
     else:
         old_dict.update(new_dict)
 
-def defaultkwargs(dict_name='_defaults'):
+def defaultkwargs(default_name='_defaults'):
+    ''' Compare a function's keyword arguments to a dictionary of defaults
+        
+        Arguments:
+        default_name -- The name of a dictionary which contains default
+        values. If @defaultkwargs is wrapping a method of class A, then
+        A.default_name should be a valid (prefereably class-level)
+        attribute
+
+        Usage:
+        Assume @defaultkwargs() is wrapping some method of class A
+        called A.func. When A.func(**kwargs) is called, @defaultkwargs
+        will copy A._defaults to a dictionary new_kwargs, update new_kwargs
+        with kwargs, and call A.func(**new_kwargs) instead. The update is
+        performed with _restrictive_update(), so that any keys in kwargs
+        not also in A._defaults will raise an error.
+
+    '''
     
     def decorate(f):
 
         @wraps(f)
         def wrapper(self, *args, **kwargs):
-            new_kwargs = dict(self.__getattribute__(dict_name)) # Make a deep copy
+            new_kwargs = dict(self.__getattribute__(default_name)) # Make a deep copy
             _restrictive_update(new_kwargs, kwargs)
             return f(self, *args, **new_kwargs)
 
